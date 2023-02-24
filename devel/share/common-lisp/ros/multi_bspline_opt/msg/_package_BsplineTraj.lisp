b2e11cef49fb08f6d538c7a790d62be163ff9da3
singingbird-svg <2319719359@qq.com>
2023-02-24 18:09:43 +0800

    去掉了bspline的时钟，但是代价函数有点问题

---
+++ devel/share/common-lisp/ros/multi_bspline_opt/msg/_package_BsplineTraj.lisp
@@ -0,0 +1,16 @@
+(cl:in-package multi_bspline_opt-msg)
+(cl:export '(HEADER-VAL
+          HEADER
+          POSITION-VAL
+          POSITION
+          VELOCITY-VAL
+          VELOCITY
+          ACCELERATION-VAL
+          ACCELERATION
+          YAW-VAL
+          YAW
+          YAW_RATE-VAL
+          YAW_RATE
+          CURRENT_SEQ-VAL
+          CURRENT_SEQ
+))
